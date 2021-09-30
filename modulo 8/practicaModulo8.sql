445aad1f2f0b6255ee53f4bd16a521904155d2bb
Alexis <alex10o@hotmail.com>
2021-09-30 20:25:11 -0300

    30-09 base de datos 3

--- modulo 8/practicaModulo8.sql
+++ modulo 8/practicaModulo8.sql
@@ -1,3 +1,9 @@
 use cafetito;
 insert into perosnas (nombre, apellido, edad ) values ('Tito', 'Cabrera', 10);
+insert into perosnas (nombre, apellido, edad ) values ('Negra', 'Cabrera', 11);
+insert into perosnas (nombre, apellido, edad ) values ('hormiga', 'Cabrera', 5);
+update perosnas set edad = 15 where idperosnas = 3;
+delete from perosnas where idperosnas = 3;
 select *from perosnas;
+rename table perosnas to personas;
+select *from personas;
