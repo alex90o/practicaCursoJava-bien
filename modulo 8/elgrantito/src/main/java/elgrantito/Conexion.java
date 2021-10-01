b40427987205f31dbe365b7a491fb6cd9ac2759c
Alexis <alex10o@hotmail.com>
2021-10-01 18:25:51 -0300

    1-10 conexion me da erros otra ves con Mysql probare con maria DB y cree main

--- modulo 8/elgrantito/src/main/java/elgrantito/Conexion.java
+++ modulo 8/elgrantito/src/main/java/elgrantito/Conexion.java
@@ -8,13 +8,14 @@ import com.mysql.cj.jdbc.Driver;
 public class Conexion {
 public static Connection obtConexion() throws Exception{
 	//nombre de driver
-	String dbDriver = "com.mysql.jdbc.Driver";
+	//String dbDriver = "com.mysql.jdbc.Driver";
+	String dbDriver = "com.mysql.cj.jdbc.Driver";
 	//para establecer la url y la base de datos
 	String dbUrlyBase = "jdbc:mysql://localhost/cafetito";
 	//usario por lo general root
 	String dbUser = "root";
 	//contraseña por lo general vacias si nose cambio
-	String dbContasenia= " ";
+	String dbContasenia= "";
 	// establece el driver de conexion
 	Class.forName(dbDriver).newInstance();
 	
--- modulo 8/elgrantito/src/main/java/elgrantito/Main.java
+++ modulo 8/elgrantito/src/main/java/elgrantito/Main.java
@@ -1,10 +1,17 @@
 package elgrantito;
 
+import java.sql.Connection;
+
 public class Main {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 		// TODO Auto-generated method stub
-
+		Connection co = Conexion.obtConexion();
+		if (co == null) {
+			System.out.println("No se conecto a la Base");
+		}else {
+			System.out.println("Si se conecto a la Base");
+		}
 	}
 
 }
