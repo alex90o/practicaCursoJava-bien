514ff5374827f9e457a5a7510f3e11e97da1d97c
Alexis <alex10o@hotmail.com>
2021-10-01 18:16:31 -0300

    1-10 conexion

--- modulo 8/elgrantito/src/main/java/elgrantito/Conexion.java
+++ modulo 8/elgrantito/src/main/java/elgrantito/Conexion.java
@@ -1,5 +1,27 @@
 package elgrantito;
 
-public class Conexion {
+import java.sql.Connection;
+import java.sql.DriverManager;
 
+import com.mysql.cj.jdbc.Driver;
+
+public class Conexion {
+public static Connection obtConexion() throws Exception{
+	//nombre de driver
+	String dbDriver = "com.mysql.jdbc.Driver";
+	//para establecer la url y la base de datos
+	String dbUrlyBase = "jdbc:mysql://localhost/cafetito";
+	//usario por lo general root
+	String dbUser = "root";
+	//contraseña por lo general vacias si nose cambio
+	String dbContasenia= " ";
+	// establece el driver de conexion
+	Class.forName(dbDriver).newInstance();
+	
+	//Retorna conexion
+	Connection conex = DriverManager.getConnection(dbUrlyBase, dbUser, dbContasenia);
+	return conex;
+	
+	
+}
 }
