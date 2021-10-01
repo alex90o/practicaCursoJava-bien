42196cc012cf01a99eeffd454e0c8b37a0474bc4
Alexis <alex10o@hotmail.com>
2021-10-01 18:48:08 -0300

    1-10 creando clase PersonaDAO

--- modulo 8/elgrantito/src/main/java/elgrantito/PersonaDAO.java
+++ modulo 8/elgrantito/src/main/java/elgrantito/PersonaDAO.java
@@ -1,5 +1,15 @@
 package elgrantito;
 
+import java.sql.Connection;
+
 public class PersonaDAO {
 
+	public static void insertar(Pesona per) throws Exception{
+		Connection co = Conexion.obtConexion();
+		String sql = "insert into personas (nombre, apellido, edad)"
+				+ "value ('"+per.getNombre()+"', '"+per.getApellido()+ "', "+
+				per.getEdad()+")";
+		
+		
+	}
 }
